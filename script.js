f0d818ca8e041894e94bef6881cc95e76b6a3209
Rishav Shah <99394765+Rishav840@users.noreply.github.com>
2024-06-29 14:36:45 +0000

    add js for unlimited menu link a

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+// const burgerMenu = document.querySelector('.burger-menu');
+// const menuList = document.querySelector('.menu-list');
+
+// burgerMenu.addEventListener("click",()=>{
+//   burgerMenu.classList.toggle("active");
+//   menuList.classList.toggle("show");
+// });
+
 const burgerMenu = document.querySelector('.burger-menu');
 const menuList = document.querySelector('.menu-list');
 
-burgerMenu.addEventListener("click",()=>{
-  burgerMenu.classList.toggle("active");
-  menuList.classList.toggle("show");
+burgerMenu.addEventListener(("click"), () => {
+    burgerMenu.classList.toggle("active");
+    menuList.classList.toggle("show");
+
+    if (menuList.classList.contains('show')) {
+        menuList.style.maxHeight = menuList.scrollHeight + 'px';
+    } else {
+        menuList.style.maxHeight = '0';
+    }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
